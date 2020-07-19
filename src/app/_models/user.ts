21c71924bef88a6df6b97e3dd5f1d7681722b447
negarnegma <negarzist@gmail.com>
2020-07-19 22:51:37 +0430

    init

---
+++ src/app/_models/user.ts
@@ -0,0 +1,12 @@
+export class User {
+    id: number;
+    username: string;
+    password: string;
+    firstName: string;
+    lastName: string;
+    
+    email: string;
+    gender: string;
+
+    token: string;
+}
